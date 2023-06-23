0799d422df74d7b79f9e8fb335a5b16c0c0a6f79
Toby Liu <ybot1122@gmail.com>
2023-06-22 18:45:40 -0700

    Start setting up the chatbot UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,42 @@ import * as css from "./App.css";
 import classNames from "classnames";
 import { SentimentAnalysisSection } from "./components/SentimentAnalysis/SentimentAnalysis";
 
+type Message = {
+  from: 'user' | 'bot',
+  text: string;
+}
+
 const App = () => {
+  const messages = React.useRef<Message[]>([]);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [numMessages, setNumMessages] = React.useState(0);
+  const [lastUserMsg, setLastUserMsg] = React.useState("");
+
+
+  const onSubmit = React.useCallback((e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (inputRef?.current?.value) {
+      setLastUserMsg(inputRef.current.value);
+    }
+
+    return false;
+  }, [])
+
+
   return (
     <div className={classNames(themeClass, css.app)}>
       <header className={css.title}>
         <h1>Deburgger</h1>
       </header>
         <div id="front-cover" className={css.frontCover}>
-          Hi I am Deburgger
+          Deburgger - The Smartest AI When it Comes to Burgers. Not much else.
         </div>
-        <SentimentAnalysisSection />
+
+        <form onSubmit={onSubmit}>
+        <input type="text" ref={inputRef} /><button type="submit">Submit</button>
+        </form>
+        <SentimentAnalysisSection text={lastUserMsg} />
     </div>
   );
 };
--- src/components/SentimentAnalysis/SentimentAnalysis.tsx
+++ src/components/SentimentAnalysis/SentimentAnalysis.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { analyzeSentiment } from "../../api/analyzeSentiment";
 import * as css from './SentimentAnalysis.css'
-import JSONPretty from 'react-json-pretty';const SentimentAnalysisSection = () => {
-  const sentimentAnalysisInput = React.useRef<HTMLInputElement>(null);
+import JSONPretty from 'react-json-pretty';
+
+const SentimentAnalysisSection = ({text}: {text: string}) => {
   const [results, setResults] = React.useState({});
 
-  const onSubmitSentimentAnalysis = React.useCallback(async () => {
-    if (sentimentAnalysisInput?.current?.value) {
-      const r = await analyzeSentiment(sentimentAnalysisInput?.current?.value);
-      setResults(r);
+  React.useEffect(() => {
+    const analyze = async () => {
+        const r = await analyzeSentiment(text);
+        setResults(r);
     }
-  }, []);
+    if (text) {
+      analyze();
+    }
+
+  }, [text])
 
   return (
     <div className={css.container}>
       <h2 className={css.header}>Sentiment Analysis</h2>
-      <div className={css.header}>
-        <input type="text" ref={sentimentAnalysisInput} /><button type="submit" onClick={onSubmitSentimentAnalysis}>Submit</button>
-        
-      </div>
       <div className={css.header}>
         <JSONPretty data={results} />
       </div>
