c07dd9327b7a22b9d5e326700436db05cfd7bfc6
Toby Liu <ybot1122@gmail.com>
2023-06-23 19:52:07 -0700

    Finish script to build excel

---
+++ sources/known_burgers/prompts.js
@@ -0,0 +1,10 @@
+// ${item} to be replace by item_name and "the" item_name and "a" and "an"
+
+module.exports = [
+  "Where can I get ${item}?",
+  "Which restaurant makes ${item}?",
+  "Who makes ${item}?",
+  "Which place makes ${item}?",
+  "Where can ${item} by found?",
+  "Where can ${item} be made?"
+]
